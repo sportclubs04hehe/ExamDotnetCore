33c15052fc234d3b74a1ff37b81313500d8939eb
w3schools-test <vegakinvietnam@gmail.com>
2022-12-17 13:23:49 +0700

    Nộp bài kiểm tra

---
+++ Employees/EmployeesWeb/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeesWeb.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Display(Name = "Employee ID")]
+        public int EmployeeID { get; set; }
+        [Required]
+        [StringLength(60)]
+        [Display(Name = "Employee Name")]
+        public String EmployeeName { get; set; }
+        [Required]
+        public String Department { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public double Salary { get; set; }
+    }
+}
